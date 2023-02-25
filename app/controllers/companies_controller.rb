7ede0c01fc62eb21c18389258bfc4db2eaf52e23
Daichi-appy <daichi.es47@gmail.com>
2023-02-25 23:29:48 +0900

    企業登録まで

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,2 +1,34 @@
 class CompaniesController < ApplicationController
+  def index
+    @companies = Company.all
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      flash.now.notice = "企業を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_company
+      @user = Company.find(params[:id])
+    end
+
+    def company_params
+      params.require(:company).permit(:name, :description)
+    end
 end
